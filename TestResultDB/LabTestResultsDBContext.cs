3bb8809cc42d52a0575c5ad7931de22c382e7b59
go-gru <64829932+go-gru@users.noreply.github.com>
2020-05-26 12:54:36 +0200

    Refactor/db (#7)
    * refactor: DB to lib
    
    * refactor: add DB lib project
    
    Co-authored-by: Gordon Grund <gordon.grund@outlook.de>

---
+++ TestResultDB/LabTestResultsDBContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace CWA.TestResultDB
+{
+    public partial class LabTestResultsDBContext : DbContext
+    {
+        public LabTestResultsDBContext()
+        {
+        }
+
+        public LabTestResultsDBContext(DbContextOptions<LabTestResultsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Models.TestResultEntity> TestResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Server=tcp:labresultsdbsrv.database.windows.net,1433;Initial Catalog=LabResultsDB;Persist Security Info=False;User ID=dbadmin;Password=pV/L65AqYS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.TestResultEntity>(entity =>
+            {
+                entity.ToTable("test_results");
+
+                entity.HasIndex(e => e.ResultId)
+                    .HasName("IX_test_results")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Result).HasColumnName("result");
+
+                entity.Property(e => e.ResultDate)
+                    .HasColumnName("result_date")
+                    .HasColumnType("timestamp");
+
+                entity.Property(e => e.ResultId)
+                    .IsRequired()
+                    .HasColumnName("result_id")
+                    .HasMaxLength(128);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
