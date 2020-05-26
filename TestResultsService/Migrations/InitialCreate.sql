9dd93c8c95adad4af0745993eebefb2c9aafa3cc
hw <holger.wilke@gmx.de>
2020-05-26 12:18:59 +0200

    feature: InitialCreate migration changed to postgresql

---
+++ TestResultsService/Migrations/InitialCreate.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20200526101104_InitialCreate') THEN
+    CREATE TABLE test_results (
+        id uuid NOT NULL,
+        result_id character varying(128) NOT NULL,
+        result_date timestamp NOT NULL,
+        result integer NOT NULL,
+        CONSTRAINT "PK_test_results" PRIMARY KEY (id)
+    );
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20200526101104_InitialCreate') THEN
+    CREATE UNIQUE INDEX "IX_test_results" ON test_results (result_id);
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20200526101104_InitialCreate') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20200526101104_InitialCreate', '3.1.4');
+    END IF;
+END $$;
