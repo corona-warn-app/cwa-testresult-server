3aa8578151b4986d6521a54f56d11dc4a0b73a98
Julien Hagestedt <julien.hagestedt@gmail.com>
2020-05-30 10:49:23 +0200

    feat: spring actuator and security

--- src/main/java/app/coronawarn/testresult/TestResultApplication.java
+++ src/main/java/app/coronawarn/testresult/TestResultApplication.java
@@ -26,10 +26,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Slf4j
 @EnableScheduling
 @EnableJpaAuditing
+@EnableWebSecurity
 @SpringBootApplication
 public class TestResultApplication {
 
