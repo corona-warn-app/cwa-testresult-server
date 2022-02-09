ba65959b0de9ff8a032d9f1ae1c337362008931a
Felix Dittrich <Felix.Dittrich@t-systems.com>
2022-02-09 17:55:01 +0100

    Add Shedlock Config

--- src/main/java/app/coronawarn/testresult/TestResultApplication.java
+++ src/main/java/app/coronawarn/testresult/TestResultApplication.java
@@ -22,6 +22,7 @@
 package app.coronawarn.testresult;
 
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -30,6 +31,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @Slf4j
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 @EnableJpaAuditing
 @EnableWebSecurity
 @SpringBootApplication
