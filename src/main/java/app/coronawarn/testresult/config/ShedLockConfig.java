7c1cf052f8f0cf24505814c6206975f7320288ae
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2022-02-09 18:07:38 +0100

    Fix: Missing Shedlock Config (#120)
    * Add Shedlock Config
    
    * Update pom.xml

---
+++ src/main/java/app/coronawarn/testresult/config/ShedLockConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Corona-Warn-App / cwa-testresult-server
+ *
+ * (C) 2020, T-Systems International GmbH
+ *
+ * Deutsche Telekom AG and all other contributors /
+ * copyright owners license this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package app.coronawarn.testresult.config;
+
+import static net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider.Configuration.builder;
+
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class ShedLockConfig {
+
+  private final DataSource dataSource;
+
+  /**
+   * Creates a LockProvider for ShedLock.
+   *
+   * @return LockProvider
+   */
+  @Bean
+  public LockProvider lockProvider() {
+    return new JdbcTemplateLockProvider(builder()
+      .withTableName("shedlock")
+      .withJdbcTemplate(new JdbcTemplate(dataSource))
+      .usingDbTime()
+      .build()
+    );
+  }
+}
