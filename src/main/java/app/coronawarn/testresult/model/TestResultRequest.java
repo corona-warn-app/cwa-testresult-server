f43fec3642f6b09af4c7a713f3b61a8221224337
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2022-02-23 10:42:39 +0100

    Update License (#122)

--- src/main/java/app/coronawarn/testresult/model/TestResultRequest.java
+++ src/main/java/app/coronawarn/testresult/model/TestResultRequest.java
@@ -1,7 +1,7 @@
 /*
  * Corona-Warn-App / cwa-testresult-server
  *
- * (C) 2020, T-Systems International GmbH
+ * (C) 2020 - 2022, T-Systems International GmbH
  *
  * Deutsche Telekom AG and all other contributors /
  * copyright owners license this file to you under the Apache
